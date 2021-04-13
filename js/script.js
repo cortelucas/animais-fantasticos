9c3c20f40fc0e13d76a05b10b7a63e3b89b1fd18
Lucas S Corte <corte1994.lc@gmail.com>
2021-04-13 13:29:07 -0300

    - estilização e codificacao para accordion

--- js/script.js
+++ js/script.js
@@ -1,19 +1,42 @@
-const tabMenu = document.querySelectorAll('#tab-menu li');
-const tabContent = document.querySelectorAll('#tab-content section');
-
-if (tabMenu.length && tabContent.length){
-  tabContent[0].classList.add('ativo');
+function initTabNav() {
+  const tabMenu = document.querySelectorAll('#tab-menu li');
+  const tabContent = document.querySelectorAll('#tab-content section');
   
-  function activeTab(i) {
-    tabContent.forEach((section) => {
-      section.classList.remove('ativo');
-    });
-    tabContent[i].classList.toggle('ativo');
+  if (tabMenu.length && tabContent.length){
+    tabContent[0].classList.add('ativo');
+    
+    function activeTab(i) {
+      tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+      });
+      tabContent[i].classList.toggle('ativo');
+    }
+    
+    tabMenu.forEach((itemMenu, i) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(i);
+      });
+    })
   }
-  
-  tabMenu.forEach((itemMenu, i) => {
-    itemMenu.addEventListener('click', () => {
-      activeTab(i);
-    });
-  })
 }
+initTabNav();
+
+function initAccordion() {  
+  const accordionList = document.querySelectorAll('#accordion dt');
+  const ativoClass = 'ativo';
+
+  if(accordionList.length) {
+    accordionList[0].classList.add(ativoClass);
+    accordionList[0].nextElementSibling.classList.toggle(ativoClass);
+    
+    function activeAccordion() {
+      this.classList.toggle(ativoClass);
+      this.nextElementSibling.classList.toggle(ativoClass);
+    }
+    
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    });
+  }  
+}
+initAccordion();
