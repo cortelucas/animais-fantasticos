f12adebf63e0a7ccbb518b077e391872e0d69200
Lucas S Corte <corte1994.lc@gmail.com>
2021-04-13 13:49:48 -0300

    animações em js e css

--- js/script.js
+++ js/script.js
@@ -40,3 +40,31 @@ function initAccordion() {
   }  
 }
 initAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('#menu a[href^="#"]');
+  
+  function scrollToSection(e) {
+    e.preventDefault();
+    const href = e.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+  
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+    /*
+    const top = section.offsetTop;
+    window.scrollTo({
+      top: top,
+      behavior: 'smooth',
+    });
+    */
+    
+  }
+  
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });  
+}
+initScrollSuave();
